e68d86a87e2185c2509297444752258ac063fd19
trs-dev <taras2017ua@gmail.com>
2019-07-30 10:26:21 +0300

    Cloned ClassicMVVM to ReactiveUI_MVVM

---
+++ ReactiveUI_MVVM/Models/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveUI_MVVM.Models
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string _FirstName;
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                if (value == _FirstName) return;
+                _FirstName = value;
+                OnPropertyChanged();
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        private string _LastName;
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                if (value == _LastName) return;
+                _LastName = value;
+                OnPropertyChanged();
+                OnPropertyChanged("FullName");
+            }
+        }
+
+
+        public Person(string FirstName, string LastName)
+        {
+            _FirstName = FirstName;
+            _LastName = LastName;
+        }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+
+        }
+
+
+        public static List<Person> GetPersons()
+        {
+            var result = new List<Person> {
+                new Person("John", "Smith"),
+                new Person("Bill", "Gates"),
+                new Person("Max", "Luis")
+            };
+            return result;
+        }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ ReactiveUI_MVVM/ViewModels/PersonViewModel.cs
@@ -0,0 +1,48 @@
+﻿using ReactiveUI_MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ReactiveUI_MVVM.ViewModels
+{
+    internal class PersonViewModel : INotifyPropertyChanged
+    {
+
+        Person _SelectedPerson;
+
+        public Person SelectedPerson
+        {
+            get { return _SelectedPerson; }
+            set
+            {
+                _SelectedPerson = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public ObservableCollection<Person> Persons { get; private set; }
+
+
+        public PersonViewModel()
+        {
+            Persons = new ObservableCollection<Person>(Person.GetPersons());
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
